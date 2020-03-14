ee906a1a81565bbac1e99f750e2517f6b575c390
AL <promstar1156@gmail.com>
2020-03-14 22:49:51 +0200

    [-] Delete unnecessary code

--- src/pages/Layout/DashboardLayout.vue
+++ src/pages/Layout/DashboardLayout.vue
@@ -32,10 +32,7 @@
         <md-icon>notifications</md-icon>
         <p>Notifications</p>
       </sidebar-link>
-      <sidebar-link to="/upgrade" class="active-pro">
-        <md-icon>unarchive</md-icon>
-        <p>Upgrade to PRO</p>
-      </sidebar-link>
+
     </side-bar>
 
     <div class="main-panel">
