9532ad5b18cc3c9ac09422c6f5bbd3b93e117e0f
= <=>
2020-10-09 16:00:34 +0300

    Fix timezone ang monthly graph

--- api/app/Http/Controllers/Scan/getMonthlyController.php
+++ api/app/Http/Controllers/Scan/getMonthlyController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Scan;
 
 use App\Http\Controllers\Controller;
 use App\Logic\Helper;
+use App\Scan;
+use App\Website;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -17,7 +19,12 @@ class getMonthlyController extends Controller
     public function __invoke(Request $request)
     {
         $user = auth()->user();
-        $website = $user->websites()->find($request->website_id);
+        if ($user->is_admin) {
+            $website = Website::find($request->website_id);
+        } else {
+            $website = $user->websites()->find($request->website_id);
+        }
+
         $date = Carbon::today()->subDays(29);
         $scans = $website->scans()->where('created_at', '>=', $date)->get()->groupBy(function($row) {
             return Carbon::parse($row->created_at)->format('d.m');
