9d9f25c34c5afc54fee7007ac244e15aee268ce9
AL <promstar1156@gmail.com>
2020-10-20 17:13:51 +0300

    Fix deploy

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -3,18 +3,22 @@ const fs = require("fs");
 
 (async () => {
   try {
-    await execa("git", ["checkout", "gh-pages"]);
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
     console.log("Building...");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
+    const CNAME = fs.writeFile("dist/CNAME", "www.watchdog-app.ml", function() {
+      if (err) throw err;
+      console.log("It's saved!");
+    });
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
-    await execa("git", ["push", "gh-pages", "--force"]);
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     //await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "main"]);
-    //await execa("git", ["branch", "-D", "gh-pages"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed");
   } catch (e) {
     console.log(e.message);
