0800313d16ed183e34a265c32bd4c2fa4e0f0b31
AL <promstar1156@gmail.com>
2021-01-11 11:28:43 +0200

    404 page rework

--- src/pages/Service/NotFound.vue
+++ src/pages/Service/NotFound.vue
@@ -22,27 +22,41 @@
       <div class="md-layout-item md-lay md-size-100  ">
         <div class="not_found_container">
           <div class="md-display-4 headline_404">
-            <md-button
-                    v-if="back_visible"
-                    v-on:click="$router.back()"
-                    style="background-color: transparent !important;border: 1px solid white; font-size: 0.4em"
-                    class="md-raised btn-transparent"
-            >
-              <md-icon style="font-size: 1em !important; margin-right: 15px;">keyboard_backspace</md-icon>Back</md-button>
+            <transition name="block" mode="out-in">
+              <md-button
+                      v-if="back_visible"
+                      v-on:click="$router.back()"
+                      style="background-color: transparent !important;font-size: 0.4em"
+                      class="md-raised btn-transparent"
+              >
+                <md-icon style="font-size: 1em !important; margin-right: 15px;">keyboard_backspace</md-icon>Back</md-button>
+            </transition>
           </div>
-          <div class="md-display-4 headline_404 middle_block" >
-            <div style="margin: auto;">404</div>
+          <div class="md-display-4 headline_  404 middle_block" >
+            <transition name="block" mode="out-in">
+              <div v-if="content_visible" style="margin: auto; color: white!important">404</div>
+            </transition>
           </div>
           <div class="md-display-4 headline_404 " >
-            <md-button
-                    v-if="home_visible"
-                    v-on:click="$router.back()"
-                    style="background-color: transparent !important;border: 1px solid white; font-size: 0.4em; float: right"
-                    class="md-raised btn-transparent"
-            >
-              Home <md-icon style="font-size: 1em !important; margin-left: 15px;">arrow_right_alt</md-icon></md-button>
+            <transition name="block" mode="slideLeft">
+              <md-button
+                      v-if="home_visible"
+                      v-on:click="$router.back()"
+                      style="background-color: transparent !important; font-size: 0.4em; float: right"
+                      class="md-raised btn-transparent"
+              >
+                Home <md-icon style="font-size: 1em !important; margin-left: 15px;">arrow_right_alt</md-icon></md-button>
+            </transition>
           </div>
         </div>
+      </div>
+      <div class="md-layout md-lay md-size-100">
+        <transition name="block" mode="slideLeft">
+          <div v-if="subheader_visible" class="md-layout-item md-alignment-bottom-center md-lay md-size-100  ">
+            <p class="text-center" style="color: white!important">Page not found</p>
+          </div>
+        </transition>
+
       </div>
     </div>
   </div>
@@ -57,7 +71,9 @@
       return {
         startTyping: false,
         home_visible: false,
-        back_visible: false
+        back_visible: false,
+        content_visible: false,
+        subheader_visible: false
       }
     },
     props: {
@@ -67,10 +83,7 @@
       }
     },
     methods: {
-      startTypingSubheader: function () {
-        //document.querySelectorAll(".typed-cursor")[0].style.display = "none";
-        this.startTyping = true;
-      }
+
     },
     computed: {
       bgImage() {
@@ -78,11 +91,32 @@
           backgroundImage: `url(${this.backgroundImage})`
         };
       }
+    },
+    mounted() {
+      const delay = t => new Promise(resolve => setTimeout(resolve, t));
+
+      delay(500).then(() => this.content_visible = true);
+      delay(1000).then(() => this.subheader_visible = true);
+      delay(1500).then(() => this.back_visible = true);
+      delay(1500).then(() => this.home_visible = true);
     }
   };
 </script>
 
 <style scoped>
+  .block-enter {
+    Opacity: 0;
+  }
+  .block-enter-active {
+    transition : opacity 1s;
+  }
+  .block-leave {
+  }
+  .block-leave-active {
+    transition: opacity 1s;
+    Opacity: 0;
+  }
+
   .middle_block {
     text-align: center;
   }
